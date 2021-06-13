c675f72825d3e95aa9ed07a9ebf514c36b1b37a4
Anu8690 <anuragsinghal@iitg.ac.in>
2021-06-13 19:28:32 +0530

    slight change

--- lib/main.dart
+++ lib/main.dart
@@ -5,8 +5,6 @@ import 'package:provider/provider.dart';
 import 'package:firebase_auth/firebase_auth.dart';
 import 'package:firebase_core/firebase_core.dart';
 
-// import 'package:permission_handler/permission_handler.dart';
-
 Future<void> main() async {
   WidgetsFlutterBinding.ensureInitialized();
   await Firebase.initializeApp();
