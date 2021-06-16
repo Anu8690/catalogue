8205c70f81dd62184ef6982bc0af7533958c7121
Anu8690 <anuragsinghal@iitg.ac.in>
2021-06-17 01:33:15 +0530

    drawer added

--- lib/screens/home/productList.dart
+++ lib/screens/home/productList.dart
@@ -15,46 +15,86 @@ class ProductList extends StatefulWidget {
 
 class _ProductListState extends State<ProductList> {
   final FirebaseAuth _auth = FirebaseAuth.instance;
-
+  int _selectedDestination = 0;
   @override
   Widget build(BuildContext context) {
     return StreamProvider<List<Product>>.value(
       initialData: [],
       value: DatabaseService().products,
       child: Scaffold(
-        
         drawer: Drawer(
-            child: ListView(
-          children: <Widget>[
-            DrawerHeader(
-              decoration: BoxDecoration(
-                color: Colors.blue,
+          child: ListView(
+            // Important: Remove any padding from the ListView.
+            padding: EdgeInsets.zero,
+            children: <Widget>[
+              Padding(
+                padding: const EdgeInsets.fromLTRB(10, 40, 0, 5),
+                child: Text(
+                  'Settings',
+                  style: Theme.of(context).textTheme.headline6,
+                ),
               ),
-              child: Text('Settings'),
-            ),
-          ],
-        )),
+              Divider(
+                height: 1,
+                thickness: 1,
+              ),
+              ListTile(
+                leading: Icon(Icons.add),
+                title: Text('Add Product'),
+                selected: _selectedDestination == 0,
+                onTap: () {
+                  selectDestination(0);
+                  Navigator.of(context).push(
+                      MaterialPageRoute(builder: (context) => ProductUpload()));
+                },
+              ),
+              ListTile(
+                leading: Icon(Icons.search),
+                title: Text('Search Product'),
+                selected: _selectedDestination == 1,
+                onTap: () => selectDestination(1),
+              ),
+              ListTile(
+                leading: Icon(Icons.label),
+                title: Text('Generate Bill'),
+                selected: _selectedDestination == 2,
+                onTap: () => selectDestination(2),
+              ),
+              Divider(
+                height: 1,
+                thickness: 1,
+              ),
+              Padding(
+                padding: const EdgeInsets.all(16.0),
+                child: Text(
+                  'Label',
+                ),
+              ),
+              ListTile(
+                leading: Icon(Icons.logout),
+                title: Text('Log Out'),
+                selected: _selectedDestination == 3,
+                onTap: () {
+                  selectDestination(3);
+                  _auth.signOut();
+                },
+              ),
+            ],
+          ),
+        ),
         backgroundColor: Colors.green[50],
         appBar: AppBar(
           title: Text('Catalogue'),
           backgroundColor: Colors.green[600],
-          actions: <Widget>[
-            IconButton(
-                onPressed: () {
-                  print("button pressed");
-                  Navigator.of(context).push(
-                      MaterialPageRoute(builder: (context) => ProductUpload()));
-                },
-                icon: Icon(Icons.add)),
-            IconButton(
-                onPressed: () {
-                  _auth.signOut();
-                },
-                icon: Icon(Icons.logout))
-          ],
         ),
         body: ProductTiles(),
       ),
     );
+  }
+
+  void selectDestination(int index) {
+    setState(() {
+      _selectedDestination = index;
+    });
   }
 }
